0dff8a04052a36c32abd971764c090f5b5d99a3e
Enzoait <pcenzox25@gmail.com>
2025-03-23 00:06:35 +0100

    file within workspace retrieving system

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,68 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "securecommit" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "securecommit" is now active!');
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    "securecommit.scanWorkspace",
+    async () => {
+      vscode.window.showInformationMessage("Scanning workspace...");
+      const workspaceFolders: readonly vscode.WorkspaceFolder[] | undefined =
+        vscode.workspace.workspaceFolders;
+      if (workspaceFolders == undefined) {
+        vscode.window.showInformationMessage(
+          "No workspace found. You need to open a folder to use SecureCommit."
+        );
+        return;
+      }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('securecommit.secureCommit', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showErrorMessage('Hello okkkkkkkkk from SecureCommit!');
-	});
+      const rootPath = workspaceFolders[0].uri.fsPath;
+      console.log("Root Path: ", rootPath);
 
-	context.subscriptions.push(disposable);
+      const files: vscode.Uri[] = await vscode.workspace.findFiles(
+        "**/*.{env,txt,c,cpp,cc,h,rs,go,java,kt,kts,scala,cs,py,rb,php,pl,pm,sh,js,mjs,ts,dart,ex,exs,erl,beam,hs,clj,cljs,cljc,fs,fsi,fsx}",
+        "**/node_modules/**"
+      );
+
+      console.log("Number of files within the workspace: ", files.length);
+
+      for (const file of files) {
+        const filePath = file.fsPath;
+
+        try {
+          const content = fs.readFileSync(filePath, "utf-8");
+
+          console.log(`File: ${filePath}`);
+          console.log(`Content:\n${content.substring(0, 200)}...`);
+        } catch (error) {
+          console.error(`Error reading file ${filePath}:`, error);
+        }
+      }
+
+      vscode.window.showInformationMessage("Workspace scan completed!");
+    }
+  );
+
+  /*
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    "securecommit.secureCommit",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage("SecureCommit is operational.");
+      vscode.workspace
+        .openTextDocument("C:\\Users\\enzoa\\OneDrive\\Documents\\test.js")
+        .then((doc) => {
+          console.log("Text content : ", doc.getText());
+        });
+    }
+  );*/
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
